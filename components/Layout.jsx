51b8cb49df9e02e130b0971429627a4bc3bfcd95
Omri Vardi <omri.vardi@scylladb.com>
2021-10-21 17:44:24 +0100

    layout fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,8 +8,10 @@ function Layout({ children }) {
     <main className="bg-gray-100 dark:bg-gray-800 rounded-2xl h-screen overflow-hidden relative">
       <div className="flex items-start justify-between">
         <Sidebar />
-        <Navbar />
-        {children}
+        <div className="w-full pl-0 md:p-4 md:space-y-4">
+          <Navbar />
+          {children}
+        </div>
       </div>
     </main>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ import ClusterSelectorDropdown from "components/ClusterSelectorDropdown";
 
 function Navbar({}) {
   return (
-    <div className="flex flex-col w-full pl-0 md:p-4 md:space-y-4">
+    <div className="flex flex-col">
       <header className="w-full shadow-lg bg-white dark:bg-gray-700 items-center h-16 rounded-2xl z-40">
         <div className="relative z-20 flex flex-col justify-center h-full px-3 mx-auto flex-center">
           <div className="relative items-center pl-1 flex w-full lg:max-w-68 sm:pr-2 sm:ml-0">
