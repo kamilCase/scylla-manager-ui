c9cfaa32393c61542b229567863a408bdc043f1e
kamilCase <kamil.kupczynski@gmail.com>
2021-10-21 14:06:23 +0200

    Adjust config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors,
-    },
+    extend: {},
   },
-  variants: {},
-  plugins: [require('tailwindcss-filters')],
-  // xwind options
-  xwind: {
-    mode: 'objectstyles',
+  variants: {
+    extend: {},
   },
+  plugins: [],
 }
