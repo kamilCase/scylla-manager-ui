74c9ed177b74d0ec1ed41a94de18dce81581b2ec
kamilCase <kamil.kupczynski@gmail.com>
2021-10-22 11:41:25 +0200

    Add active page indicator

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -5,6 +5,18 @@ import { useRouter } from "next/router";
 function Sidebar() {
   const router = useRouter();
   const { clusterId } = router.query;
+
+  function getClassesForRouteLink(subroute) {
+    const activeLinkClasses =
+      "text-blue-500 bg-gradient-to-r from-white to-blue-100 border-r-4 border-blue-500 dark:from-gray-700 dark:to-gray-800 border-r-4 border-blue-500";
+    const inactiveLinkClasses =
+      "text-gray-500 dark:text-gray-200 hover:text-blue-500";
+
+    return router.asPath.includes(subroute)
+      ? activeLinkClasses
+      : inactiveLinkClasses;
+  }
+
   return (
     <div className="h-screen hidden lg:block my-4 ml-4 shadow-lg relative w-80">
       <div className="bg-white h-full rounded-2xl dark:bg-gray-700">
@@ -15,7 +27,9 @@ function Sidebar() {
           <div>
             <Link href={`/cluster/${clusterId}/status`}>
               <a
-                className="w-full font-thin uppercase text-blue-500 flex items-center p-4 my-2 transition-colors duration-200 justify-start bg-gradient-to-r from-white to-blue-100 border-r-4 border-blue-500 dark:from-gray-700 dark:to-gray-800 border-r-4 border-blue-500"
+                className={`w-full font-thin uppercase flex items-center p-4 my-2 transition-colors duration-200 justify-start ${getClassesForRouteLink(
+                  "status"
+                )}`}
                 href="#"
               >
                 <span className="text-left">
@@ -34,7 +48,9 @@ function Sidebar() {
             </Link>
             <Link href={`/cluster/${clusterId}/backup`}>
               <a
-                className="w-full font-thin uppercase text-gray-500 dark:text-gray-200 flex items-center p-4 my-2 transition-colors duration-200 justify-start hover:text-blue-500"
+                className={`w-full font-thin uppercase flex items-center p-4 my-2 transition-colors duration-200 justify-start ${getClassesForRouteLink(
+                  "backup"
+                )}`}
                 href="#"
               >
                 <span className="text-left">
@@ -54,7 +70,9 @@ function Sidebar() {
             </Link>
             <Link href={`/cluster/${clusterId}/terminal`}>
               <a
-                className="w-full font-thin uppercase text-gray-500 dark:text-gray-200 flex items-center p-4 my-2 transition-colors duration-200 justify-start hover:text-blue-500"
+                className={`w-full font-thin uppercase flex items-center p-4 my-2 transition-colors duration-200 justify-start ${getClassesForRouteLink(
+                  "terminal"
+                )}`}
                 href="#"
               >
                 <span className="text-left">
---
+++ pages/cluster/[clusterId]/terminal.js
@@ -0,0 +1,13 @@
+import Layout from "components/Layout";
+
+function ClusterTerminalPage() {
+  return (
+    <Layout>
+      <h1 className="text-6xl font-normal leading-normal mt-0 mb-2 text-blue-400">
+        <span className="font-bold">TERMINAL PAGE</span>
+      </h1>
+    </Layout>
+  );
+}
+
+export default ClusterTerminalPage;
